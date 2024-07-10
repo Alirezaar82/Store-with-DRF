4ec04345b1f076f43675382b3c912c3a3a33bc1a
Alirezaar82 <153992724+Alirezaar82@users.noreply.github.com>
2024-07-10 23:53:36 +0330

    set product model and category

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
+class CategoryModel(models.Model):
+    name = models.CharField(verbose_name=_('name'),max_length=255)
+    slug = models.SlugField(verbose_name=_('slug'),max_length=255)
+
+    created_at = models.DateTimeField(verbose_name=_('created'),auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name=_('update'),auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+class ProductStatusType(models.IntegerChoices):
+    published = 1,_('published') 
+    draft = 2,_('draft') 
+
+class ProductModel(models.Model):
+    title = models.CharField(verbose_name=_('title'),max_length=255)
+    slug = models.SlugField(verbose_name=_('slug'),max_length=255, blank=True,unique=True,allow_unicode=True)
+    category = models.ForeignKey(CategoryModel,on_delete=models.CASCADE,verbose_name=_('category'),)
+    description = models.TextField(verbose_name=_('description'))
+    image = models.ImageField(verbose_name=_('image'),upload_to='shop/',default='shop/default.png/')
+    
+    price = models.DecimalField(verbose_name=_('price'),max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(verbose_name=_('stock'),)
+    status = models.IntegerField(verbose_name=_('status'),choices=ProductStatusType.choices, default=ProductStatusType.draft.value)
+    
+    created_at = models.DateTimeField(verbose_name=_('created'),auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name=_('update'),auto_now=True)
+    
+    def __str__(self):
+        return self.title
